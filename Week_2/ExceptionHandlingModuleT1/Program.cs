7f41aaaa64f6ae7bf27c6b411f09f156a39635dc
VladKRP <vladislavqlm@gmail.com>
2018-03-19 13:16:00 +0300

    Done with Task 1 in Exception Handling Module

---
+++ Week_2/ExceptionHandlingModuleT1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExceptionHandlingModuleT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+                System.Console.WriteLine("Type something:");
+                var userInput = Console.ReadLine();
+                if(!string.IsNullOrWhiteSpace(userInput))
+                    System.Console.WriteLine(userInput[0]);
+                else 
+                    System.Console.WriteLine("User input must contain at least one character.\n" +
+                                             "We suppose you pass empty string.");
+            }
+        }
+    }
+}
