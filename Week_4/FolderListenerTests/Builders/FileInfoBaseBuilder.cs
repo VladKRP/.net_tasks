6b9bab859033821eda07150959c56b1cebb9ded8
VladKRP <vladislavqlm@gmail.com>
2018-03-28 16:55:00 +0300

    Add file system entities builder to simplify creation flow

---
+++ Week_4/FolderListenerTests/Builders/FileInfoBaseBuilder.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderListenerTests.Builders
+{
+    public class FileInfoBaseBuilder
+    {
+        private Mock<FileInfoBase> _fileinfoMock;
+
+        public FileInfoBaseBuilder()
+        {
+            _fileinfoMock = new Mock<FileInfoBase>();
+        }
+
+        public FileInfoBaseBuilder SetName(string name) {
+            _fileinfoMock.Setup(x => x.Name).Returns(name);
+            return this;
+        }
+
+        public FileInfoBaseBuilder SetFullName(string fullName)
+        {
+            _fileinfoMock.Setup(x => x.FullName).Returns(fullName);
+            return this;
+        }
+
+        public FileInfoBase Build()
+        {
+            _fileinfoMock.Setup(x => x.Extension).Returns(Path.GetExtension(_fileinfoMock.Object.Name));
+            return _fileinfoMock.Object;
+        }
+    }
+}
---
+++ Week_4/FolderListenerTests/Builders/FileSystemBuilder.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderListenerTests.Builders
+{
+    public class FileSystemBuilder
+    {
+        private Mock<IFileSystem> _fileSystemMock;
+
+        public FileSystemBuilder()
+        {
+            _fileSystemMock = new Mock<IFileSystem>();
+        }
+
+        public FileSystemBuilder AddDirectoryInfo(DirectoryInfoBase directoryInfo)
+        {
+            _fileSystemMock.Setup(x => x.DirectoryInfo.FromDirectoryName(directoryInfo.FullName)).Returns(() => directoryInfo);
+            return this;
+        }
+
+        public FileSystemBuilder AddDirectoriesInfo(params DirectoryInfoBase[] directoryInfos)
+        {
+            foreach (var directoryInfo in directoryInfos)
+                AddDirectoryInfo(directoryInfo);
+            return this;
+        }
+
+        public IFileSystem Build()
+        {
+            return _fileSystemMock.Object;
+        }
+    }
+}
