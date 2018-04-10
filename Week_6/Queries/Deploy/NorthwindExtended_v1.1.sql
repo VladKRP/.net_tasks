92413e101bb77f75a72046291b9be790128b48cc
VladKRP <vladislavqlm@gmail.com>
2018-04-10 18:50:34 +0300

    Add versioning scripts

---
+++ Week_6/Queries/Deploy/NorthwindExtended_v1.1.sql
@@ -0,0 +1,29 @@
+﻿
+USE Northwind;
+
+GO
+
+IF(EXISTS(SELECT * FROM SYSOBJECTS WHERE NAME='EmployeesCreditCards' AND xtype='U'))
+	DROP TABLE Northwind.EmployeesCreditCards
+GO
+
+CREATE TABLE Northwind.EmployeesCreditCards (
+	CardNumber INT IDENTITY(1,1) NOT NULL,
+	CardExpireDate DATE NOT NULL,
+	CardHolder NVARCHAR(30),
+	EmployeeID INT,
+	CONSTRAINT PK_CardNumber PRIMARY KEY (
+		CardNumber
+	),
+	CONSTRAINT FK_EmployeeID FOREIGN KEY(
+		EmployeeID
+	) REFERENCES Northwind.Employees (
+		EmployeeID
+	)
+)
+
+GO
+
+CREATE INDEX CardNumber ON Northwind.EmployeesCreditCards(CardNumber)
+
+GO
---
+++ Week_6/Queries/Deploy/NorthwindExtended_v1.3.sql
@@ -0,0 +1,11 @@
+﻿USE Northwind;
+GO
+
+IF EXISTS(SELECT * FROM SYSOBJECTS WHERE NAME = 'Region' AND xtype ='U')
+	EXEC SP_RENAME 'Northwind.Region', 'Regions'
+GO
+
+IF EXISTS(SELECT * FROM SYSOBJECTS WHERE NAME = 'Customers' AND xtype='U')
+	ALTER TABLE [Northwind].[Customers] 
+		ADD FoundationDate DATE NULL
+GO
