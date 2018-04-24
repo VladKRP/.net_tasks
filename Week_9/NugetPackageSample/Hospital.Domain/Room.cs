69bb720c8133948c52507205835e1c4b2c0171a7
VladKRP <vladislavqlm@gmail.com>
2018-04-24 13:16:28 +0300

    End-up with point 2.1 in task

---
+++ Week_9/NugetPackageSample/Hospital.Domain/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Domain
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        public string RoomNumber { get; set; }
+
+        public HospitalRoomType RoomType { get; set; }
+
+        public int Capacity { get; set; }
+
+        public IEnumerable<Person> Persons { get; set; }
+    }
+
+    public enum HospitalRoomType
+    {
+        NotSpecified,
+        HospitalStaff,
+        Patient
+    }
+}
