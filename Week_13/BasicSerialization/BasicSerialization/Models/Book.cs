323c5176af438d974472ac0e6cdef59b0273cb75
VladKRP <vladislavqlm@gmail.com>
2018-05-04 13:41:26 +0300

    Rename folder with project. End-up with basic serialization task.

---
+++ Week_13/BasicSerialization/BasicSerialization/Models/Book.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BasicSerialization.Models
+{
+    [Serializable]
+    [XmlType(TypeName = "book")]
+    public class Book
+    {
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlElement(ElementName = "isbn")]
+        public string Isbn { get; set; }
+        [XmlElement(ElementName = "author")]
+        public string Author { get; set; }
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+
+        //Little cheat
+        [XmlElement(ElementName = "genre")]
+        public string _genre;
+
+        [XmlIgnore]
+        public Genre Genre
+        {
+            get
+            {
+                var genreWithoutSpaces = _genre?.Replace(" ", "");
+                if (Genre.TryParse(genreWithoutSpaces, out Genre genre))
+                    return genre;
+                else
+                    return Genre.None;
+            }
+        }
+        //
+
+        //[XmlElement(ElementName = "genre")]
+        //public Genre Genre { get; set; }
+
+        [XmlElement(ElementName = "publisher")]
+        public string Publisher { get; set; }
+        [XmlElement(ElementName = "publish_date")]
+        public DateTime PublishDate { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "registration_date")]
+        public DateTime RegistrationDate { get; set; }
+
+
+        public Book()
+        {
+
+        }
+    }
+}
---
+++ Week_13/BasicSerialization/BasicSerialization/Models/Genre.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace BasicSerialization.Models
+{
+    // In this case ScienceFiction rise exception cause value has space
+    //[Flags] 
+    //[Serializable]
+    //public enum Genre
+    //{
+    //    None = 1,
+    //    [XmlEnum]
+    //    Computer = 2,
+    //    [XmlEnum]
+    //    Fantasy = 4,
+    //    [XmlEnum]
+    //    Romance = 6,
+    //    [XmlEnum]
+    //    Horror = 8,
+    //    [XmlEnum("Science Fiction")]
+    //    ScienceFiction = 16
+    //}
+
+    [Flags]
+    [Serializable]
+    public enum Genre
+    {
+        None = 1,
+        Computer = 2,
+        Fantasy = 4,
+        Romance = 6,
+        Horror = 8,
+        ScienceFiction = 16
+    }
+
+}
