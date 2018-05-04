9bf80d14054c211e85af4ce8a23434a77b9fd796
VladKRP <vladislavqlm@gmail.com>
2018-05-04 15:59:38 +0300

    Add solution for fibonachi in-process cache

---
+++ Week_12/FibonachiCache/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using StackExchange.Redis;
+
+namespace FibonachiCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MemoryCache memoryCache = new MemoryCache("fibo-cache");
+
+            CalculateFibonachiNumber(4, memoryCache);
+            CalculateFibonachiNumber(4, memoryCache);
+            CalculateFibonachiNumber(13, memoryCache);
+            CalculateFibonachiNumber(13, null);
+        }
+
+        public static int CalculateFibonachiNumber(int n, MemoryCache cache)
+        {
+            var cachedValue = cache?.Get(n.ToString()) as Nullable<int>;
+            if (cachedValue.HasValue)
+                    return cachedValue.Value;
+
+            var result = CalculateFibonachiNumber(n);
+
+            if(cache != null)
+                cache.Add(new CacheItem(n.ToString(), result), new CacheItemPolicy());
+
+            return result;
+        }
+
+        public static int CalculateFibonachiNumber(int n)
+        {
+            if (n <= 2)
+                return 1;
+            int current = 1;
+            int previous = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                current += previous;
+                previous = current - previous;
+            }
+
+            return current;
+        }
+    }
+}
