dd9d80e3703e7a95ba1e13ceecd589742722dba9
VladKRP <vladislavqlm@gmail.com>
2018-05-07 16:30:43 +0300

    Fix employee serialization problem

--- Week_12/Samples/Application/NorthwindLibrary/Employee.cs
+++ Week_12/Samples/Application/NorthwindLibrary/Employee.cs
@@ -5,6 +5,7 @@ namespace NorthwindLibrary
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Runtime.Serialization;
     using System.Xml.Serialization;
 
     public partial class Employee
@@ -69,15 +70,12 @@ namespace NorthwindLibrary
         [StringLength(255)]
         public string PhotoPath { get; set; }
 
-        [XmlIgnore]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Employee> Employees1 { get; set; }
-        [XmlIgnore]
+        [IgnoreDataMember]
         public virtual Employee Employee1 { get; set; }
-        [XmlIgnore]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Order> Orders { get; set; }
-        [XmlIgnore]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Territory> Territories { get; set; }
     }
