f8db8e1552834e77a0b0cfd6b55943ebdb425c83
VladKRP <vladislavqlm@gmail.com>
2018-05-18 16:18:51 +0300

    Change database schema for vesrions v1.0 and 1.3

---
+++ Week_6/Northwind/Northwind_v1.3/dbo/Tables/EmployeeCreditCards.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[EmployeeCreditCards] (
+    [CardNumber] NVARCHAR (50) NOT NULL,
+    [CardHolder] NVARCHAR (50) NULL,
+    [ExpireDate] DATETIME      NOT NULL,
+    [EmployeeID] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([CardNumber] ASC),
+    CONSTRAINT [FK_EmployeeCard_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employees] ([EmployeeID])
+);
+
