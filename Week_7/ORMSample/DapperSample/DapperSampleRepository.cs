bd035d05a5373ae71dab4c1610627dcaf6dbedae
VladKRP <vladislavqlm@gmail.com>
2018-05-21 17:37:37 +0300

    Move comparer to another file

--- Week_7/ORMSample/DapperSample/DapperSampleRepository.cs
+++ Week_7/ORMSample/DapperSample/DapperSampleRepository.cs
@@ -9,6 +9,7 @@ using Dapper;
 using ORMSample.Domain;
 using Dapper.Contrib.Extensions;
 using ORMSample.Interfaces;
+using DapperSamples;
 
 namespace ORMSample
 {
@@ -131,35 +132,7 @@ namespace ORMSample
 
     }
 
-        class SupplierEqualityComparer : IEqualityComparer<Supplier>
-        {
-            public bool Equals(Supplier firstSupplier, Supplier secondSupplier)
-            {
-                bool isEqual = false;
-                if(firstSupplier != null && secondSupplier != null)
-                {
-                    if (firstSupplier.SupplierID != secondSupplier.SupplierID)
-                        return false;
-
-                    var firstSupplierType = firstSupplier.GetType();
-                    var secondSupplierType = secondSupplier.GetType();
-
-                    if (!firstSupplierType.Equals(secondSupplierType))
-                        return false;
-
-                    var properties = firstSupplierType.GetProperties();
-                    if (properties.All(property => object.Equals(property.GetValue(firstSupplier),property.GetValue(secondSupplier))))
-                        isEqual = true;
-                }
-                return isEqual;
-                
-            }
-
-            public int GetHashCode(Supplier obj)
-            {
-                return obj.SupplierID + base.GetHashCode();
-            }
-        }
+        
 
         public void AddEmployeeWithTerritories(Employee employee)
         {
